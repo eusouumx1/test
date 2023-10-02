ecd6a12a49653ec51985b5bfd0994ae66bb04647
T-GAMER <T-GAMER@DESKTOP-HO7UDGJ>
2023-10-01 22:15:46 -0300

    Adicionar arquivos de projeto.

---
+++ test/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Alunos
+    {
+        private int matricula;
+        public string nome;
+        internal double prova1;
+        internal double prova2;
+        internal double Trabalho;
+    }
+    
+}
---
+++ test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Xml;
+using test;
+
+namespace fgfgfg
+{
+
+    public class Program
+    {
+
+        
+        static void Main()
+        {
+
+            Alunos Lucas = new Alunos()
+            {
+                nome = "Lucas",
+                prova1 = 10,
+                prova2 = 10,
+                Trabalho = 10,
+
+            };
+
+
+            Console.WriteLine(Math.Round(MediaCalculator(Lucas.prova2, Lucas.prova1, Lucas.Trabalho), 2));
+
+        }
+        public static double MediaCalculator(double prova1, double prova2, double Trabalho)
+            {
+            double MediaResultado = (prova1*2.5 + prova2*2.5 + Trabalho*2) / 3 ;
+
+            return MediaResultado;
+            }
+                  
+    }
+}
+
+
+
